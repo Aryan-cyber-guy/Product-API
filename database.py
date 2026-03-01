71e31f32d7e4df6c4538a00676bd63325c0a193c
Aryan-cyber-guy <flyhigh7674@gmail.com>
2026-03-01 11:22:19 +0530

    DB url

--- database.py
+++ database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-db_url = "postgresql://postgres:1234@localhost:5432/fastapi_db"
-engine = create_engine(db_url)
+DATABASE_URL = os.getenv(
+    "postgresql://product_db_p5ew_user:wdzQAkaoGaocbFn3Koc5pRI6LSk6530H@dpg-d6ht1eogjchc73cumf80-a/product_db_p5ew",
+    "postgresql://postgres:password@localhost:5432/mydb"
+)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}  # Required for Render
+)
 session = sessionmaker(autoflush=False,autocommit=False,bind=engine)
